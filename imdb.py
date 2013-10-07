9bd24f11c03c71a621bc3255b9899a18ea94b019
Akash Nimare <svnitakash@gmail.com>
2013-10-08 03:38:53 +0530

    Removed error

--- imdb.py
+++ imdb.py
@@ -7,7 +7,6 @@ def imdb():
   t = raw_input("Enter movie: ")
   url = 'http://deanclatworthy.com/imdb/?'
   return url+urllib.urlencode({"q":t})
-print imdb()
 fetch = urllib.urlopen(imdb())
 data = json.load(fetch)
 print data["rating"]
